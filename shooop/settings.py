ad3e53f65a8c9f1e47ffe5e5157dd363a64fa99c
AkshayMozhikkal <akshay.for.career@gmail.com>
2023-07-03 09:38:42 +0530

    Commit After ENV

--- shooop/settings.py
+++ shooop/settings.py
@@ -13,6 +13,7 @@ import os
 import pytz
 from django.utils import timezone
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hrf2x&)fk6jpkx2$@)^q+rbn=n%ai0y3m083)yt2d4&oo^nu6('
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'shooop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shooop',
-        'USER': 'postgres',
-        'PASSWORD': 'Akson@123',
+        'NAME': config('dbname'),
+        'USER': config('dbuser'),
+        'PASSWORD': config('dbpass'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -128,12 +128,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Email Notification 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'emailcollector.django@gmail.com'
-EMAIL_HOST_PASSWORD = 'pulbfgzitoxwycjd'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
