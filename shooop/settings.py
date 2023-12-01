5465a07be5aae2643886f8e309c038456062bf03
AkshayMozhikkal <akshay.for.career@gmail.com>
2023-12-01 19:35:24 +0530

    Middleware removed

--- shooop/settings.py
+++ shooop/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'shooop.middleware.LogoutRedirectMiddleware',
+    # 'shooop.middleware.LogoutRedirectMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
